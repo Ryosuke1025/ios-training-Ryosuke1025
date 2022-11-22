d953c5ec85e2c477ddbce372eb506da9c7796242
Ryosuke1025 <ryosuke131025@icloud.com>
2022-11-22 14:18:38 +0900

    [Add]AutoLayoutの追加

--- ios-training-Ryosuke1025/ViewController.swift
+++ ios-training-Ryosuke1025/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttounHStack: UIStackView!
+    @IBOutlet weak var constraints: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let imageWidth = self.view.frame.size.width / 2
+        constraints.constant = imageWidth
+        buttounHStack.spacing = 20
+    }
 
 }
 
